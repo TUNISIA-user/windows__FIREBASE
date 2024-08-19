0d2247aa34a454927d3c157c09db2f4bd39374de
Ghaith Nahdi <121437369+TUNISIA-user@users.noreply.github.com>
2024-08-19 11:23:05 +0100

    Update firebase123.js
    @point

--- src/firebase123.js
+++ src/firebase123.js
@@ -11,7 +11,7 @@ const firebaseConfig = {
   messagingSenderId: "9483947953",
   appId: "1:9483947953:web:f64108e2083e7a30bdf790"
 };
-
+//      
   const app = initializeApp(firebaseConfig)
   export const auth = getAuth(app)
   const db = getFirestore(app)
